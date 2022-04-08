4b6fce54eca72564984b918e74f15e3ff78237de
jason.lotz <jason.lotz@ibm.com>
2022-04-08 17:02:15 -0400

    Added .vscode to .gitignore.

--- 8/rotate.py
+++ 8/rotate.py
@@ -1,6 +1,7 @@
 def rotate(string, n):
     """
-    Rotate characters in a string.
-       Expects string and n (int) for number of characters to move"""
+      Rotate characters in a string.
+      Expects string and n (int) for number of characters to move
+   """
 
     return string[n:] + string[:n]
