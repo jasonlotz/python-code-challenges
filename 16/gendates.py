1d255e7a6e488669d1904ae9c3112d343a75f567
jason.lotz <jason.lotz@ibm.com>
2022-04-10 20:20:42 -0400

    Refactored 16 to be a generator pattern.

--- 16/gendates.py
+++ 16/gendates.py
@@ -4,14 +4,7 @@ PYBITES_BORN = datetime(year=2016, month=12, day=19)
 
 
 def gen_special_pybites_dates():
-
-    result = []
-
-    for n in range(1, 11):
-        new_date = PYBITES_BORN + timedelta(days=n * 100)
-        result.append(new_date)
-
-    return result
-
-
-print(gen_special_pybites_dates())
+    date = PYBITES_BORN
+    while True:
+        date += timedelta(days=100)
+        yield date
