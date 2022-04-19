f0494e78beee073647d086963d244cc8d9b49ab0
jason.lotz <jasonlotz@gmail.com>
2022-04-19 13:50:31 -0400

    Finished 44.

---
+++ 44/license.py
@@ -0,0 +1,28 @@
+# use one or more Standard Library modules
+import random
+import string
+
+
+def gen_key(parts: int = 4, chars_per_part: int = 8) -> str:
+    """
+    Generate and return a random license key containing
+    upper case characters and digits.
+
+    Example with default "parts" and "chars_per_part"
+    being 4 and 8: KI80OMZ7-5OGYC1AC-735LDPT1-4L11XU1U
+
+    If parts = 3 and chars_per_part = 4 a random license
+    key would look like this: 54N8-I70K-2JZ7
+    """
+
+    characters = string.ascii_uppercase + string.digits
+
+    result = ''
+    for part in range(0, parts):
+        result = result + ''.join(random.choice(characters)
+                                  for i in range(chars_per_part))
+
+        if part + 1 < parts:
+            result = result + '-'
+
+    return result
