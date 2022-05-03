b940caeee0a356d35786e39d714bda1c826dd6c2
Jason Lotz <jasonlotz@gmail.com>
2022-05-03 10:32:57 -0400

    Refactored 91.

--- 91/anyall.py
+++ 91/anyall.py
@@ -1,3 +1,5 @@
+import re
+
 VOWELS = 'aeiou'
 PYTHON = 'python'
 DIGITS = '1234567890'
@@ -12,10 +14,12 @@ def contains_only_vowels(input_str):
 def contains_any_py_chars(input_str):
     """Receives input string and checks if any of the PYTHON
        chars are in it. Match is case insensitive."""
-    return any([char in PYTHON for char in input_str.lower()])
+    #  return any([char in PYTHON for char in input_str.lower()])
+    return re.search(rf'[{PYTHON}]', input_str.lower())
 
 
 def contains_digits(input_str):
     """Receives input string and checks if it contains
        one or more digits."""
-    return any([char in DIGITS for char in input_str])
+    #  return any([char in DIGITS for char in input_str])
+    return re.search(r'\d+', input_str)
