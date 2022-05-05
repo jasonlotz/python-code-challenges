a971c33a1e0b49953143d7bf2ab0b01935e17609
Jason Lotz <jasonlotz@gmail.com>
2022-05-05 11:03:29 -0400

    Refactored 100.

--- 100/tail.py
+++ 100/tail.py
@@ -9,13 +9,8 @@ def tail(filepath: Path, n: int) -> List[str]:
     - Parse it into a list of lines, stripping trailing newlines.
     - Return the last "n" lines.
     """
-    result = []
 
     with open(filepath) as file:
 
-        # loop to read iterate
-        # last n lines and print it
-        for line in (file.readlines()[-n:]):
-            result.append(line.rstrip('\n'))
-
-    return result
+        lines = [line.rstrip() for line in file.readlines()[-n:]]
+        return lines
