6d5eacfe0736dfcfd4e63ecc3bd1449ee0d4c5e2
Jason Lotz <jasonlotz@gmail.com>
2022-05-15 14:17:16 -0400

    Finished 161.

---
+++ 161/tree.py
@@ -0,0 +1,14 @@
+import os
+
+
+def count_dirs_and_files(directory='.'):
+    """Count the amount of of directories and files in passed in "directory" arg.
+       Return a tuple of (number_of_directories, number_of_files)
+    """
+    dirs_count = 0
+    files_count = 0
+    for root, dirs, files in os.walk(directory):
+        dirs_count += len(dirs)
+        files_count += len(files)
+
+    return (dirs_count, files_count)
