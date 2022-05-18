33e16a349915ec4186270ce0543a2d58c75eb89f
Jason Lotz <jasonlotz@gmail.com>
2022-05-18 10:30:27 -0400

    Finished 169.

---
+++ 169/convert.py
@@ -0,0 +1,16 @@
+def convert(value: float, fmt: str) -> float:
+    """Converts the value to the designated format.
+
+    :param value: The value to be converted must be numeric or raise a TypeError
+    :param fmt: String indicating format to convert to
+    :return: Float rounded to 4 decimal places after conversion
+    """
+    if not isinstance(value, int) and not isinstance(value, float):
+        raise TypeError
+
+    if fmt.lower() == 'cm':
+        return round(value * 2.54, 4)
+    elif fmt.lower() == 'in':
+        return round(value / 2.54, 4)
+    else:
+        raise ValueError
