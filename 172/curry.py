51fde5fe9ce197077d6c70e86699964d7a1b8852
Jason Lotz <jasonlotz@gmail.com>
2022-05-19 10:31:31 -0400

    Fixed imports for 172.

--- 172/curry.py
+++ 172/curry.py
@@ -4,5 +4,5 @@ import functools
 # create 2 partials:
 # - 'rounder_int' rounds to int (0 places)
 # - 'rounder_detailed' rounds to 4 places
-rounder_int = functools.partial(round, ndigits=0)
-rounder_detailed = functools.partial(round, ndigits=4)
+rounder_int = partial(round, ndigits=0)
+rounder_detailed = partial(round, ndigits=4)
