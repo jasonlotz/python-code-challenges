de04c3f00a33f71d85e24b7901e99067a0ed91b5
Jason Lotz <jasonlotz@gmail.com>
2022-05-27 11:17:47 -0400

    Finished 209.

---
+++ 209/docstring.py
@@ -0,0 +1,9 @@
+def sum_numbers(numbers):
+    """Sums numbers
+    :param numbers: a list of numbers
+    :type list
+    :raises TypeError: if not all numeric values passed in
+    :return: sum of numbers
+    :rtype: int
+    """
+    pass
