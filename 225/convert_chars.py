297caf7c69ff0aac3e391ee40097f0450b4b32b7
Jason Lotz <jasonlotz@gmail.com>
2022-05-31 18:39:43 -0400

    Added additional one liner for 225.

--- 225/convert_chars.py
+++ 225/convert_chars.py
@@ -4,6 +4,9 @@ PYBITES = "pybites"
 def convert_pybites_chars(text):
     """Swap case all characters in the word pybites for the given text.
        Return the resulting string."""
+
+    # Alternative one liner
+    # return ''.join(c.swapcase() if c.lower() in PYBITES.lower() else c for c in text)
     result = ''
 
     for char in text:
