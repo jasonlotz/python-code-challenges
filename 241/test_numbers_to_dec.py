640493006ee7cba29dadafb130b46a1cf4b98b54
Jason Lotz <jasonlotz@gmail.com>
2022-06-03 10:35:06 -0400

    Finished 241.

--- 241/test_numbers_to_dec.py
+++ 241/test_numbers_to_dec.py
@@ -2,19 +2,12 @@ import pytest
 
 from numbers_to_dec import list_to_decimal
 
-#    for num in nums:
-#         if isinstance(num, bool) or not isinstance(num, int):
-#             raise TypeError
-#         elif not num in range(0, 10):
-#             raise ValueError
-
-#     return int(''.join(map(str, nums)))
-
 
 @pytest.mark.parametrize('input, expected', [
     ([0, 4, 2, 8], 428),
     ([1, 2], 12),
     ([3], 3),
+    ([0], 0)
 ])
 def test_fib(input, expected):
     assert list_to_decimal(input) == expected
@@ -40,6 +33,11 @@ def test_negative():
         list_to_decimal([-5, ])
 
 
-def test__too_big():
+def test_10():
     with pytest.raises(ValueError) as e:
-        list_to_decimal([11, ])
+        list_to_decimal([10, ])
+
+
+def test_big():
+    with pytest.raises(ValueError) as e:
+        list_to_decimal([100, ])
