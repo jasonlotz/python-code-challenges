3c7bb9e469918357da0e4c5b8980a31d3280cfe3
Jason Lotz <jasonlotz@gmail.com>
2022-06-05 14:49:05 -0400

    Finished 251.

---
+++ 251/series.py
@@ -0,0 +1,46 @@
+import string
+
+import pandas as pd
+import numpy as np
+
+
+def basic_series() -> pd.Series:
+    """Create a pandas Series containing the values 1, 2, 3, 4, 5
+    Don't worry about the indexes for now.
+    The name of the series should be 'Fred'
+    """
+    return pd.Series([1, 2, 3, 4, 5], dtype='int64', name='Fred')
+
+
+def float_series() -> pd.Series:
+    """Create a pandas Series containing the all the values
+    from 0.000 -> 1.000 e.g. 0.000, 0.001, 0.002... 0.999, 1.000
+    Don't worry about the indexes or the series name.
+    """
+    data = [x for x in np.arange(0, 1.0001, 0.001)]
+
+    return pd.Series(data, dtype='float64')
+
+
+def alpha_index_series() -> pd.Series:
+    """Create a Series with values 1, 2, ... 25, 26 of type int64
+    and add an index with values a, b, ... y, z
+    so index 'a'=1, 'b'=2 ... 'y'=25, 'z'=26
+    Don't worry about the series name.
+    """
+    data = [x for x in np.arange(1, 27)]
+    indices = [i for i in string.ascii_lowercase]
+
+    return pd.Series(data, dtype='int64', index=indices)
+
+
+def object_values_series() -> pd.Series:
+    """Create a Series with values A, B, ... Y, Z of type object
+    and add an index with values 101, 102, ... 125, 126
+    so index 101='A', 102='B' ... 125='Y', 126='Z'
+    Don't worry about the series name.
+    """
+    data = [i for i in string.ascii_uppercase]
+    indices = [x for x in np.arange(101, 127)]
+
+    return pd.Series(data, dtype='object', index=indices)
