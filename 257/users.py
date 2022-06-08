6e6a6ff61aee13cbf8e31b5bf12089aa6b8ffa67
Jason Lotz <jasonlotz@gmail.com>
2022-06-08 10:25:19 -0400

    Refactored 257.

--- 257/users.py
+++ 257/users.py
@@ -1,7 +1,5 @@
 import re
 
-PATTERN = re.compile(r'(,){2,}')
-
 
 def get_users(passwd: str) -> dict:
     """Split password output by newline,
@@ -10,18 +8,10 @@ def get_users(passwd: str) -> dict:
       replace multiple commas in name with a single space
       return dict of keys = user, values = name.
     """
-    result = {}
-
-    for line in passwd.splitlines():
-        if line:
-            split_line = line.split(':')
-
-            user = split_line[0]
-            name = split_line[4] if split_line[4] else 'unknown'
-
-            name = name.rstrip(',')
-            name = re.sub(PATTERN, ' ', name)
-
-            result[user] = name
-
-    return result
+    output = {}
+    for row in passwd.strip().splitlines():
+        fields = row.split(':')
+        username = fields[0]
+        name = re.sub(r',+', r' ', fields[4].strip(',')) or 'unknown'
+        output[username] = name
+    return output
