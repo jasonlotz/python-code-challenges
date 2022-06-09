67f7c0118aa27d919a5d4f3452308b6e52d76ed6
Jason Lotz <jasonlotz@gmail.com>
2022-06-09 10:00:00 -0400

    Added alternative solution for 262.

--- 262/gc_content.py
+++ 262/gc_content.py
@@ -16,3 +16,11 @@ def calculate_gc_content(sequence):
         counts[char.lower()] += 1
 
     return round(100 * (counts['g'] + counts['c']) / (counts['a'] + counts['c'] + counts['g'] + counts['t']), 2)
+
+    # My solution wasn't bad, but I do think a Counter was more appropriate than
+    # a defaultdict for this.
+    #
+    # counts = Counter(sequence.upper())
+    # gc_content = counts.get("G", 0) + counts.get("C", 0)
+    # at_content = counts.get("A", 0) + counts.get("T", 0)
+    # return round(gc_content * 100 / (gc_content + at_content), 2)
