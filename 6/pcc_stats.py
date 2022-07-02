9e838fa3aa5fee42fa768882effcd24284364e9d
Jason Lotz <jasonlotz@gmail.com>
2022-07-01 21:08:27 -0400

    Finished 6.

--- 6/pcc_stats.py
+++ 6/pcc_stats.py
@@ -1,56 +1,75 @@
-"""Checks community branch dir structure to see who submitted most
-   and what challenge is more popular by number of PRs"""
 from collections import Counter, namedtuple
 import os
 import urllib.request
-import re
 
 # prep
-temp_file = os.path.join('/tmp', 'dir_names')
-urllib.request.urlretrieve('http://bit.ly/2ABUTjv', temp_file)
+tmp = os.getenv("TMP", "/tmp")
+tempfile = os.path.join(tmp, 'dirnames')
+urllib.request.urlretrieve(
+    'https://bites-data.s3.us-east-2.amazonaws.com/dirnames.txt',
+    tempfile
+)
 
-IGNORE = 'static templates data pybites bbelderbos hobojoe1848'.split()
-
-users, popular_challenges = Counter(), Counter()
+IGNORE = ['static', 'templates', 'data',
+          'pybites', 'bbelderbos', 'hobojoe1848']
 
 Stats = namedtuple('Stats', 'user challenge')
 
 
 # code
 
-def gen_files():
-    """Return a generator of dir names reading in temp_file
-
-       temp_file has this format:
-
-       challenge<int>/file_or_dir<str>,is_dir<bool>
-       03/rss.xml,False
-       03/tags.html,False
-       ...
-       03/mridubhatnagar,True
-       03/aleksandarknezevic,True
-
-       -> use last column to filter out directories (= True)
+def gen_files(tempfile=tempfile):
     """
-    with open(temp_file, "r") as f:
-        for line in f:
-            if line.endswith(",False\n"):
-                continue
-            yield line
+    Parse the tempfile passed in, filtering out directory names
+    (first column) using the last "is_dir" column.
 
+    Lowercase these directory names and return them as a generator.
 
-def diehard_pybites():
-    """Return a Stats namedtuple (defined above) that contains the user that
-       made the most PRs (ignoring the users in IGNORE) and a challenge tuple
-       of most popular challenge and the amount of PRs for that challenge.
-       Calling this function on the dataset (held temp_file) should return:
-       Stats(user='clamytoe', challenge=('01', 7))
+    "tempfile" has the following format:
+    challenge<int>/file_or_dir<str>,is_dir<bool>
+
+    For example:
+    03/rss.xml,False
+    03/tags.html,False
+    03/Mridubhatnagar,True
+    03/aleksandarknezevic,True
+
+    => Here you would return 03/mridubhatnagar (lowercased!)
+       followed by 03/aleksandarknezevic
     """
-    for file in gen_files():
-        challenge, user = re.search(r"(.*?)/(.*?),", file).groups()
-        if user in IGNORE:
+    with open(tempfile) as fp:
+        for line in fp.readlines():
+            name, is_dir = line.split(",")
+            if is_dir.strip() == "True":
+                yield name.lower()
+
+
+def diehard_pybites(files=None):
+    """
+    Return a Stats namedtuple (defined above) that contains:
+    1. the user that made the most pull requests (ignoring the users in IGNORE), and
+    2. a tuple of:
+        ("most popular challenge id", "amount of pull requests for that challenge")
+
+    Calling this function on the default dirnames.txt should return:
+
+    Stats(user='clamytoe', challenge=('01', 7))
+    """
+    if files is None:
+        files = gen_files()
+
+    users = Counter()
+    popular_challenges = Counter()
+
+    # your code
+    for dir in files:
+        challenge, username = dir.split("/")
+        if username in IGNORE:
             continue
-        users[user] += 1
+
+        users[username] += 1
         popular_challenges[challenge] += 1
-    return Stats(users.most_common(1)[0][0],
-                 *popular_challenges.most_common(1))
+
+    user = users.most_common(1)[0][0]
+    pop_challenge = popular_challenges.most_common(1)[0]
+    return Stats(user=user, challenge=pop_challenge)
