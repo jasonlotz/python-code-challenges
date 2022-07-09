0052d136a7b2448ac53d3e2f50eeeb44fd8588bc
Jason Lotz <jasonlotz@gmail.com>
2022-07-09 11:07:33 -0400

    Refactored 17.

--- 17/friends.py
+++ 17/friends.py
@@ -4,6 +4,7 @@ import itertools
 
 def friends_teams(friends: list, team_size: int = 2, order_does_matter: bool = False):
     if order_does_matter:
-        return itertools.permutations(friends, team_size)
+        func = itertools.permutations
     else:
-        return itertools.combinations(friends, team_size)
+        func = itertools.combinations
+    return func(friends, team_size)
