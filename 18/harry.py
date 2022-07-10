34f265fe99ef9125231ad91984cca882e48c8a4b
Jason Lotz <jasonlotz@gmail.com>
2022-07-10 10:36:25 -0400

    Finished 18.

---
+++ 18/harry.py
@@ -0,0 +1,27 @@
+import os
+import urllib.request
+import re
+from collections import Counter
+
+# data provided
+tmp = os.getenv("TMP", "/tmp")
+stopwords_file = os.path.join(tmp, 'stopwords')
+harry_text = os.path.join(tmp, 'harry')
+urllib.request.urlretrieve(
+    'https://bites-data.s3.us-east-2.amazonaws.com/stopwords.txt',
+    stopwords_file
+)
+urllib.request.urlretrieve(
+    'https://bites-data.s3.us-east-2.amazonaws.com/harry.txt',
+    harry_text
+)
+
+
+def get_harry_most_common_word():
+    harry_words = re.sub(r'[^a-zA-Z\s*]', "",
+                         open(harry_text).read().lower()).split()
+
+    words = (word for word in harry_words if word not in open(
+        stopwords_file).read())
+
+    return Counter(words).most_common(1)[0]
