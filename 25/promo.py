98ad7380d847cd231efea14ce4d797f78c6be7bc
Jason Lotz <jasonlotz@gmail.com>
2022-07-12 09:08:36 -0400

    Finished 25.

---
+++ 25/promo.py
@@ -0,0 +1,48 @@
+import random
+
+BITES = {6: 'PyBites Die Hard',
+         7: 'Parsing dates from logs',
+         9: 'Palindromes',
+         10: 'Practice exceptions',
+         11: 'Enrich a class with dunder methods',
+         12: 'Write a user validation function',
+         13: 'Convert dict in namedtuple/json',
+         14: 'Generate a table of n sequences',
+         15: 'Enumerate 2 sequences',
+         16: 'Special PyBites date generator',
+         17: 'Form teams from a group of friends',
+         18: 'Find the most common word',
+         19: 'Write a simple property',
+         20: 'Write a context manager',
+         21: 'Query a nested data structure'}
+BITES_DONE = {6, 10, 16, 18, 21}
+
+
+class NoBitesAvailable(Exception):
+    """There are no more Bites available to pick from"""
+
+
+class Promo:
+
+    def __init__(self):
+        # updated Bite to make local copies (avoid globals!)
+        self.all_bites = BITES.copy()
+        self.bites_done = BITES_DONE.copy()
+
+    def _pick_random_bite(self):
+        bites_available = []
+        for b in BITES:
+            if b not in self.bites_done:
+                bites_available.append(b)
+        if bites_available:
+            return random.choice(bites_available)
+        else:
+            raise NoBitesAvailable
+
+    def new_bite(self):
+        if not self.bites_done:
+            raise NoBitesAvailable
+        else:
+            random_bite = self._pick_random_bite()
+            self.bites_done.add(random_bite)
+            return random_bite
