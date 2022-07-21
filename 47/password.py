a71028afb0580fabc5ff8b02f3b12a1b5589865d
Jason Lotz <jasonlotz@gmail.com>
2022-07-21 09:34:02 -0400

    Finished 47.

---
+++ 47/password.py
@@ -0,0 +1,16 @@
+import string
+import re
+
+used_passwords = set('PassWord@1 PyBit$s9'.split())
+
+
+def validate_password(password):
+    if 6 <= len(password) <= 12 and \
+            re.search(r"[0-9]", password) and \
+            len(re.findall(r"[a-z]", password)) > 1 and \
+            re.search(r"[A-Z]", password) and \
+            re.search(rf"[{string.punctuation}]", password) and \
+            password not in used_passwords:
+        used_passwords.add(password)
+        return True
+    return False
