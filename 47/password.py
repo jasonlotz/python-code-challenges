8e16cb6518ab2fe8cb0220cdd33c32b91a595638
Jason Lotz <jasonlotz@gmail.com>
2022-07-21 09:36:01 -0400

    Refactored 47.

--- 47/password.py
+++ 47/password.py
@@ -1,16 +1,29 @@
 import string
 import re
 
+PUNCTUATION_CHARS = list(string.punctuation)
+
 used_passwords = set('PassWord@1 PyBit$s9'.split())
 
 
 def validate_password(password):
-    if 6 <= len(password) <= 12 and \
-            re.search(r"[0-9]", password) and \
-            len(re.findall(r"[a-z]", password)) > 1 and \
-            re.search(r"[A-Z]", password) and \
-            re.search(rf"[{string.punctuation}]", password) and \
-            password not in used_passwords:
-        used_passwords.add(password)
-        return True
-    return False
+    if not 6 <= len(password) <= 12:
+        return False
+
+    if not re.search(r'[a-z].*[a-z]', password):
+        return False
+
+    if not re.search(r'[A-Z]', password):
+        return False
+
+    if not re.search(r'[0-9]', password):
+        return False
+
+    if not any(char in PUNCTUATION_CHARS for char in password):
+        return False
+
+    if password in used_passwords:
+        return False
+
+    used_passwords.add(password)
+    return True
