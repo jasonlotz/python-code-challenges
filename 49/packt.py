a3d5709256bc647f8c087607ffcb2f423bb21c27
Jason Lotz <jasonlotz@gmail.com>
2022-07-25 12:11:11 -0400

    Finished 49.

--- 49/packt.py
+++ 49/packt.py
@@ -11,4 +11,22 @@ Book = namedtuple('Book', 'title description image link')
 
 def get_book():
     """make a Soup object, parse the relevant html sections, and return a Book namedtuple"""
-    pass
+    soup = Soup(CONTENT, 'html.parser')
+    title = soup.find('div', {'class': 'dotd-title'}).text
+    title = title.replace('\t', '').replace('\n', '')
+    link = soup.find_all(
+        'div', {'class': 'dotd-main-book-image float-left'})[0].a.attrs['href']
+    image = soup.find_all(
+        'div', {'class': 'dotd-main-book-image float-left'})[0].noscript.img.attrs['src']
+    description = soup.find_all(
+        'div', {'class': 'dotd-main-book-summary float-left'})[0].contents[7].text
+    description = description.replace('\t', '').replace('\n', '')
+
+    book = Book(
+        title,
+        description,
+        image,
+        link
+    )
+
+    return book
