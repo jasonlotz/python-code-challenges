2b7034b1a861d345e6b6885c22104a15acf8cc93
Jason Lotz <jasonlotz@gmail.com>
2022-07-28 13:38:46 -0400

    Refactored 59.  Didn't quite understand the original instructions.

--- 59/tables.py
+++ 59/tables.py
@@ -1,29 +1,34 @@
+from itertools import product
+
+
+def pad(n): return f'{n:>3}'  # noqa E731
+
+
 class MultiplicationTable:
 
     def __init__(self, length):
         """Create a 2D self._table of (x, y) coordinates and
            their calculations (form of caching)"""
-        self._table = [[x*y for x in range(1, length+1)]
-                       for y in range(1, length+1)]
+        self.length = length
+        axis = range(1, self.length + 1)
+        self._table = {(i, j): i*j
+                       for i, j in product(axis, axis)}
 
     def __len__(self):
         """Returns the area of the table (len x* len y)"""
-        return len(self._table) * len(self._table[0])
+        return len(self._table)
 
     def __str__(self):
         """Returns a string representation of the table"""
-        result = ''
-        for row in self._table:
-            result += (' | '.join(str(x) for x in row))
-            result += '\n'
-
-        return result
+        output = []
+        for i, res in enumerate(self._table.values(), 1):
+            val = str(res)
+            val += i % self.length == 0 and '\n' or ' | '
+            output.append(val)
+        return ''.join(output)
 
     def calc_cell(self, x, y):
         """Takes x and y coords and returns the re-calculated result"""
-        if x > len(self._table) or y > len(self._table):
+        if not (x, y) in self._table:
             raise IndexError
         return x * y
-
-
-print(MultiplicationTable(10))
