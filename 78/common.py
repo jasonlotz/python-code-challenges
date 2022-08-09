5992a73998a9665e7cfc2d47c6a35c663ebcb7af
Jason Lotz <jasonlotz@gmail.com>
2022-08-09 11:42:03 -0400

    Refactored 78.

--- 78/common.py
+++ 78/common.py
@@ -4,4 +4,5 @@ from functools import reduce
 def common_languages(programmers):
     """Receive a dict of keys -> names and values -> a sequence of
        of programming languages, return the common languages"""
-    return list(reduce(set.intersection, [set(languages) for languages in programmers.values()]))
+    languages = [set(language) for language in programmers.values()]
+    return set.intersection(*languages)
