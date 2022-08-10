3d2322e22c3440e2862f2815371dffe5c0077fab
Jason Lotz <jasonlotz@gmail.com>
2022-08-10 10:39:09 -0400

    Finished 79.

--- 79/community.py
+++ 79/community.py
@@ -1,42 +1,29 @@
-from collections import defaultdict
-
 import csv
+from collections import Counter
 
 import requests
 
 CSV_URL = 'https://bites-data.s3.us-east-2.amazonaws.com/community.csv'
 
+timezones = Counter()
+
 
 def get_csv():
     """Use requests to download the csv and return the
        decoded content"""
-    result = defaultdict(list)
-    content_tuple = []
-    internal_list = []
     with requests.Session() as s:
         download = s.get(CSV_URL)
-
-        decoded_content = download.content.decode('utf-8')
-        cr = csv.reader(decoded_content.splitlines(), delimiter=',')
-
-        my_list = list(cr)
-        for row in my_list:
-            internal_list.append(row)
-
-        for c in internal_list:
-            content_tuple.append((c[2], c[1]))
-
-        for k, v in content_tuple:
-            if k != 'tz':
-                result[k].append(v)
-
-    return result
+        return download.content.decode('utf-8')
 
 
 def create_user_bar_chart(content):
     """Receives csv file (decoded) content and print a table of timezones
        and their corresponding member counts in pluses to standard output
     """
-    result = content
-    for k in sorted(result.keys()):
-        print(k.ljust(21, ' '), '|', len(result[k])*'+')
+    reader = csv.DictReader(content.splitlines(), delimiter=',')
+    for row in reader:
+        tz = row['tz']
+        timezones[tz] += 1
+
+    for location, count in sorted(timezones.items()):
+        print(f'{location:<20} | {"+"*count}')
