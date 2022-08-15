c3439c50107226e4302f540829f6cb3bc298c965
Jason Lotz <jasonlotz@gmail.com>
2022-08-15 13:29:42 -0400

    Finished 82.

---
+++ 82/score.py
@@ -0,0 +1,17 @@
+from statistics import mean
+from enum import Enum
+
+THUMBS_UP = '👍'  # in case you go f-string ...
+
+
+class Score(Enum):
+    BEGINNER = 2
+    INTERMEDIATE = 3
+    ADVANCED = 4
+    CHEATED = 1
+
+    def average():
+        return mean([score.value for score in Score])
+
+    def __str__(self):
+        return f'{self.name} => {THUMBS_UP * self.value}'
