08906046d1139e57029851ad2a1b7ba92ac0208b
Jason Lotz <jasonlotz@gmail.com>
2022-08-23 10:50:57 -0400

    Refactored 87.

--- 87/roman.py
+++ 87/roman.py
@@ -1,34 +1,30 @@
 from collections import OrderedDict
 
+number_to_numeral_mapping = OrderedDict({
+    1000: 'M',
+    900: 'CM',
+    500: 'D',
+    400: 'CD',
+    100: 'C',
+    90: 'XC',
+    50: 'L',
+    40: 'XL',
+    10: 'X',
+    9: 'IX',
+    5: 'V',
+    4: 'IV',
+    1: 'I',
+})
+
 
 def romanize(decimal_number):
-    """Takes a decimal number int and converts its Roman Numeral str"""
+    if not isinstance(decimal_number, int) or not 0 < decimal_number < 4000:
+        raise ValueError('Use an int in range 1-3999')
 
-    # If a non int or invalid number (<= 0 or >= 4000) is given raise a ValueError.
-    if not isinstance(decimal_number, int) or decimal_number <= 0 or decimal_number >= 4000:
-        raise ValueError
+    result = ''
+    for number, numeral in number_to_numeral_mapping.items():
+        while decimal_number >= number:
+            result += numeral
+            decimal_number -= number
 
-    roman = OrderedDict()
-    roman[1000] = 'M'
-    roman[900] = 'CM'
-    roman[500] = 'D'
-    roman[400] = 'CD'
-    roman[100] = 'C'
-    roman[90] = 'XC'
-    roman[50] = 'L'
-    roman[40] = 'XL'
-    roman[10] = 'X'
-    roman[9] = 'IX'
-    roman[5] = 'V'
-    roman[4] = 'IV'
-    roman[1] = 'I'
-
-    def roman_num(decimal_number):
-        for r in roman.keys():
-            x, _ = divmod(decimal_number, r)
-            yield roman[r] * x
-            decimal_number -= (r * x)
-            if decimal_number <= 0:
-                break
-
-    return ''.join([a for a in roman_num(decimal_number)])
+    return result
