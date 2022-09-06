b4200fff6028792bc2cf43b7f254ddebbda8e91c
Jason Lotz <jasonlotz@gmail.com>
2022-09-06 10:13:19 -0400

    Finished 99.

---
+++ 99/sequence_generator.py
@@ -0,0 +1,13 @@
+import string
+from itertools import islice
+
+
+def sequence_generator():
+    letters = string.ascii_uppercase
+    numbers = range(1, len(letters)+1)
+    while True:
+        i = 0
+        while i < 26:
+            yield numbers[i]
+            yield letters[i]
+            i += 1
