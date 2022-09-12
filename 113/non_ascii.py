bccc9228a4f89811be1966c232f9a48da8b50e94
Jason Lotz <jasonlotz@gmail.com>
2022-09-12 11:42:07 -0400

    Refactored 113.

--- 113/non_ascii.py
+++ 113/non_ascii.py
@@ -1,13 +1,8 @@
-from string import printable
+def _is_ascii(word):
+    """Helper to determine if a word consists of only ascii chars"""
+    return not all(ord(char) < 128 for char in word)
 
 
 def extract_non_ascii_words(text):
     """Filter a text returning a list of non-ascii words"""
-    result = []
-    p = printable
-    for word in text.split(' '):
-        for letter in word:
-            if letter not in p and word not in result:
-                result.append(word)
-
-    return result
+    return [word for word in text.split() if _is_ascii(word)]
